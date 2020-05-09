909a53bca6bfb7ec1701e2e7035d85504db11a37
airizarry2 <54568019+airizarry2@users.noreply.github.com>
2020-05-08 22:13:31 -0500

    Update TicketProcessing.java

--- TicketProcessing.java
+++ TicketProcessing.java
@@ -14,6 +14,7 @@ public class TicketProcessing extends Tickets{
 	
 	ArrayList <Tickets> ticks = new ArrayList <>();
 	
+	ArrayList <LoginT> login = new ArrayList<>();
 	
 	Scanner sc = new Scanner(System.in); 	
 		
@@ -22,15 +23,20 @@ public class TicketProcessing extends Tickets{
     String description = sc.nextLine(); 
 
     Tickets obj1 = new Tickets();
+    LoginT obj2 = new LoginT();
+    
     obj1.setTick(description);
     ticks.add(obj1);
-    obj1.setsDate(new Date(2020,5,8));
-    obj1.seteDate(new Date(2020,5,10));
-    
+    obj1.setsDate(new Date(1588896000*1000));
+    obj1.seteDate(new Date(1588993449*1000));
+    login.add(obj2);
+    obj2.setusername("username");
+    obj2.setpassword("password");
 	//Tickets br= new Tickets();
 	Dao dao = new Dao();
 	//dao.createTable();
-	dao.insertRecords(ticks); //perform inserts
+	dao.insertRecordsT(login);
+	dao.insertRecords(ticks);//perform inserts
 	//ResultSet rs= dao.retrieveRecords();//fill result set object
 	
 	
